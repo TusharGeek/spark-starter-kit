91906c68401b9356c955a5c9321f8f7144ec745a
TusharGeek <tusharanand517@gmail.com>
2024-09-08 10:37:13 +0530

    added link in nav

--- apps/cart-system/src/app/component/Navbar.tsx
+++ apps/cart-system/src/app/component/Navbar.tsx
@@ -24,6 +24,7 @@ const Navbar: React.FC = () => {
           <li><Link to="/">Home</Link></li>
           <li><Link to="/about">About</Link></li>
           <li><Link to="/contact">Contact</Link></li>
+          <li><Link to="/cart">Cart</Link></li>
         </ul>
 
         {/* Burger Menu Toggle Button */}
